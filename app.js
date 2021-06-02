49f7c463d10b32b934ca7f8721fb0ad95bfd3ce2
sangeethasr <sangeetharajan567@gmail.com>
2021-06-02 16:26:24 +0530

    Completed Product List Page

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // NAVBAR SECTION //
+
+const menuItems = document.getElementById("menuItems");
+
+menuItems.style.maxHeight = "0px";
+
+function menuToggle(){
+    if(menuItems.style.maxHeight != "0px" ){
+        menuItems.style.maxHeight = "0px";
+    }else{
+        menuItems.style.maxHeight = "200px";
+    }
+}
