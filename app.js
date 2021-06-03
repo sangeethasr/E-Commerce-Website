e79ef61c4f027ba011ab407eea4fc56638d57ba8
sangeethasr <sangeetharajan567@gmail.com>
2021-06-03 13:31:57 +0530

    Added Login/Signin Page

--- app.js
+++ app.js
@@ -11,3 +11,21 @@ function menuToggle(){
         menuItems.style.maxHeight = "200px";
     }
 }
+
+// ACCOUNT PAGE - TOGGLE FORM //
+
+const loginForm = document.getElementById("loginForm");
+const registerForm = document.getElementById("registerForm");
+const indicator = document.getElementById("indicator");
+
+function viewRegisterForm(){
+    loginForm.style.transform = "translateX(0px)";
+    registerForm.style.transform = "translateX(0px)";
+    indicator.style.transform = "translateX(50px)";
+}
+
+function viewLoginForm(){
+    loginForm.style.transform = "translateX(330px)";
+    registerForm.style.transform = "translateX(330px)";
+    indicator.style.transform = "translateX(-50px)";
+}
