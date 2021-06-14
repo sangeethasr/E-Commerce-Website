7f8fabfff667a92b2a58c60818ee1a1a5c5953fa
sangeethasr <sangeetharajan567@gmail.com>
2021-06-14 22:45:36 +0530

    Enabled add to cart option

--- productDetailsPage.js
+++ productDetailsPage.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 const productid = params.get('productid');
-console.log('productDetailsPage: ',productid);
+console.log('productDetailsPage: ', productid);
 
 var PDetailsimage = document.getElementById("PDetailsimage");
 var PDetailsHeading = document.getElementById("PDetailsHeading");
@@ -13,29 +13,49 @@ var addToCart = document.getElementById("addToCart");
 
 
 fetch('https://ecommerce-project-efa9a-default-rtdb.firebaseio.com/productList.json', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    }
 }).then(response => {
     return response.json();
-}).then(inputDbResponse =>{
+}).then(inputDbResponse => {
     console.log(inputDbResponse);
-    console.log(inputDbResponse[productid]);
 
     PDetailsimage.src = inputDbResponse[productid].image;
     PDetailsHeading.innerHTML = inputDbResponse[productid].name;
-    PDetailsPrice.innerHTML = '$ '+inputDbResponse[productid].price;
+    PDetailsPrice.innerHTML = '$ ' + inputDbResponse[productid].price;
 
     const cimage = inputDbResponse[productid].image;
     const cname = inputDbResponse[productid].name;
     const cprice = inputDbResponse[productid].price;
 
     addToCart.addEventListener('click', () => {
-        additemstoFirebase(cimage, cname, cprice);
+        additemstoFirebase(cimage, cname, cprice, productid);
     });
 
 }).catch(error => {
     console.error('catch : ', error);
 })
 
+// add items to cartlist //
+
+function additemstoFirebase(cimage, cname, cprice, productid) {
+ 
+    var cquantity = document.getElementById("PDetailsQuantity").value;
+    const cartdata = {image: cimage, name: cname, price: cprice, quantity: cquantity};
+
+    fetch('https://ecommerce-project-efa9a-default-rtdb.firebaseio.com/cartList/' + productid + '.json', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cartdata)
+    }).then(response => {
+        return response.json();
+    }).then(cartdbresponse => {
+        console.log(cartdbresponse);
+    }).catch(error => {
+        console.error('catch : ', error);
+    })
+}
